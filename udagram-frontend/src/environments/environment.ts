52fa76ad44ab3099996f2dba221190f285385124
kingsleyocran <ocrankingsley@gmail.com>
2022-07-13 02:07:28 +0000

    modified env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a958ba2fdcc1440128231a228c6776fa-1956555053.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
